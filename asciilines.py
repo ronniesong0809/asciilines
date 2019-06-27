5b651f1bcac09d5855660168155ab46db3f50b6b
ronniesong0809 <ronniesong0809@gmail.com>
2019-06-26 23:59:02 -0700

    fix: fix list index out of range error in some special cases

--- asciilines.py
+++ asciilines.py
@@ -66,12 +66,14 @@ for line in list[1:]:
   for i in range(length):
     # horizontal
     if direction == 'h':
-      if column+i < y:
-        canvas[row][column+i] = character
+      if column+i < y and row < x:
+        if column+i >= 0:
+          canvas[row][column+i] = character
     # vertical
     elif direction == 'v':
-      if row+i < x:
-        canvas[row+i][column] = character
+      if row+i < x and column < y:
+        if row+i >= 0:
+          canvas[row+i][column] = character
   # print(canvas)
 
 # display
