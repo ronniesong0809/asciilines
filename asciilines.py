77260c84b71433d1e90f5ab229b6e73b35deb096
ronniesong0809 <ronniesong0809@gmail.com>
2019-06-26 21:13:16 -0700

    feat: conver to list of list, and init the canvas

--- asciilines.py
+++ asciilines.py
@@ -6,3 +6,19 @@ file = open(filename)
 
 strings = file.readlines()
 print(strings)
+
+# convert to list of list
+list=[]
+for char in strings:
+  char_split = char.split()
+  list.append(char_split)
+# print(list)
+
+# init canvas
+x = int(list[0][0])
+y = int(list[0][1])
+# print(x,y)
+
+# fill with .
+canvas = [['.'*y]*x]
+print(canvas)
