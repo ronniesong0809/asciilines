cb2024899241059d04775f21c7eec5e845b245e0
ronniesong0809 <ronniesong0809@gmail.com>
2019-06-26 23:03:18 -0700

    feat: add some checks

--- asciilines.py
+++ asciilines.py
@@ -1,11 +1,15 @@
 import sys
 
+if len(sys.argv) != 2:
+  print("Please included a test case. USAGE: python3 asciilines.py tests/test1.tvg")
+  exit()
+
 # read from file
 filename = sys.argv[1]
 file = open(filename)
 
 strings = file.readlines()
-print(strings)
+# print(strings)
 
 # convert to list of list
 list=[]
@@ -15,6 +19,14 @@ for char in strings:
 # print(list)
 
 # init canvas
+if len(list[0])!=2:
+  print("Invalid input, the " + str(list[0]) + " sizing command need to containing only 2 characters.")
+  exit()
+
+# if list[0][0]!=int or list[0][1]!=int:
+#   print("Invalid input, the " + str(list[0]) + " rendering command need to containing [int] [int].")
+#   exit()
+
 x = int(list[0][0])
 y = int(list[0][1])
 # print(x,y)
@@ -30,11 +42,24 @@ for i in range(x):
 
 # render canvas
 for line in list[1:]:
+  if len(line)!=5:
+    print(line)
+    print("Invalid input, the " + str(line) + " rendering command need to containing only 5 characters.")
+    exit()
+
+  # if line[0]!=str or line[1]!=int or line[2]!=int or line[3]!=str or line[4]!=int:
+  #   print('Invalid input, the " + str(line) + " rendering command need to containing [char] [int] [int] [char] [int].')
+  #   exit()
+
   character = line[0] # a character to render with
   row = int(line[1]) # a row position to start at
   column = int(line[2]) # a column position to start at
   direction = line[3] # a direction of horizontal or vertical
   length = int(line[4]) # a length for the rendered line
+
+  if length<0:
+    print("Invalid length, the length [" + str(length) +"] for the rendered line need to be positive.")
+    exit()
 
   for i in range(length):
     # horizontal
@@ -42,10 +67,10 @@ for line in list[1:]:
       if column+i < y:
         canvas[row][column+i] = character
     # vertical
-    if direction == 'v':
+    elif direction == 'v':
       if row+i < x:
         canvas[row+i][column] = character
-print(canvas)
+  # print(canvas)
 
 # display
 for line in canvas:
