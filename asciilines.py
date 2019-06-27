62e2557cc6caadd5d2e205791382c90deff089d3
ronniesong0809 <ronniesong0809@gmail.com>
2019-06-26 23:32:01 -0700

    fix: fix variable type checks by using try/except

--- asciilines.py
+++ asciilines.py
@@ -23,13 +23,14 @@ if len(list[0])!=2:
   print("Invalid input, the " + str(list[0]) + " sizing command need to containing only 2 characters.")
   exit()
 
-# if list[0][0]!=int or list[0][1]!=int:
-#   print("Invalid input, the " + str(list[0]) + " rendering command need to containing [int] [int].")
-#   exit()
-
-x = int(list[0][0])
-y = int(list[0][1])
-# print(x,y)
+# https://stackoverflow.com/questions/8075877/converting-string-to-int-using-try-except-in-python
+try:
+  x = int(list[0][0])
+  y = int(list[0][1])
+  # print(x,y)
+except ValueError:
+  print("Invalid input, the " + str(list[0]) + " rendering command need to containing [int] [int].")
+  exit()
 
 # fill with .
 canvas= []
@@ -47,15 +48,16 @@ for line in list[1:]:
     print("Invalid input, the " + str(line) + " rendering command need to containing only 5 characters.")
     exit()
 
-  # if line[0]!=str or line[1]!=int or line[2]!=int or line[3]!=str or line[4]!=int:
-  #   print('Invalid input, the " + str(line) + " rendering command need to containing [char] [int] [int] [char] [int].')
-  #   exit()
-
-  character = line[0] # a character to render with
-  row = int(line[1]) # a row position to start at
-  column = int(line[2]) # a column position to start at
-  direction = line[3] # a direction of horizontal or vertical
-  length = int(line[4]) # a length for the rendered line
+  # https://stackoverflow.com/questions/8075877/converting-string-to-int-using-try-except-in-python
+  try:
+    character = line[0] # a character to render with
+    row = int(line[1]) # a row position to start at
+    column = int(line[2]) # a column position to start at
+    direction = line[3] # a direction of horizontal or vertical
+    length = int(line[4]) # a length for the rendered line
+  except ValueError:
+    print("Invalid input, the " + str(line) + " rendering command need to containing [char] [int] [int] [char] [int].")
+    exit()
 
   if length<0:
     print("Invalid length, the length [" + str(length) +"] for the rendered line need to be positive.")
